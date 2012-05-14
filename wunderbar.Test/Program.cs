2637d6b6ba3ac6b04ecaf83214d8448de989e540
Maximilian Krauss <max@kraussz.com>
2012-05-14 23:04:49 +0200

    Updated Tests

--- wunderbar.Test/Program.cs
+++ wunderbar.Test/Program.cs
@@ -3,19 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using wunderbar.Api;
+using wunderbar.Api.dataContracts;
+using System.IO;
 
 namespace wunderbar.Test {
 	class Program {
 		static void Main(string[] args) {
 
-			var client = new wunderClient {
-			                              	Credentials = new digestCredentials {eMail = testCredentials.eMail, Password = testCredentials.Password}
-			                              };
+			var client = new wunderClient();
+			client.localStorageDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "wunderbar");
+
+			Console.WriteLine("client.Login");
+			if(!client.Login(testCredentials.eMail,testCredentials.Password))
+				Console.WriteLine("Login failed!");
+
 			
-			//Console.WriteLine("Test Login:");
 			//Console.WriteLine(client.Login() ? "Success" : "Fail");
 
-			Console.WriteLine("Sync");
+			//Grab all Tasks and Lists
+			Console.WriteLine("client.Synchronize");
+			client.Synchronize();
+
+			//Add a Testtask
+			/*client.Tasks.Add(new taskType {
+				listId = 106437,
+				userId = 29253,
+				Deleted = 0,
+				Done = 0,
+				Important = 1,
+				Name = "Hi there little boggers!",
+				Id = 0,
+				Version = 0,
+				Note = ""
+			});*/
+
+			//Modify Task
+			var task = client.Tasks.First(t => t.Done == 0 && t.Deleted == 0);
+			task.Name = task.Name + " Muh!";
+			task.Version++;
+
+			//Synchronize changes
 			client.Synchronize();
 
 			Console.ReadKey();

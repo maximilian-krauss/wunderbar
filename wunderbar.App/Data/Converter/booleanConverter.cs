a9c8adca939626c0fcca0a292291715aa96adfaf
Maximilian Krauss <max@kraussz.com>
2012-05-18 21:11:41 +0200

    Added Boolean and DateTime Converter for DataBinding in TaskDialog

---
+++ wunderbar.App/Data/Converter/booleanConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace wunderbar.App.Data.Converter {
+	public sealed class booleanConverter : IValueConverter {
+		#region IValueConverter Members
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+			return ((int) value) == 1;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+			return (bool) value ? 1 : 0;
+		}
+
+		#endregion
+	}
+}
---
+++ wunderbar.App/Data/Converter/unixDateTimeConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace wunderbar.App.Data.Converter {
+	public sealed class unixDateTimeConverter : IValueConverter {
+		#region IValueConverter Members
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+			DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Local);
+			origin = origin.AddHours(TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now).Hours); //This is important because the value provided by wunderlist is an UTC Date
+			return origin.AddSeconds((long)value);
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+			if (value is DateTime) {
+				var dt = (DateTime) value;
+				if (dt.Year >= (DateTime.Now.Year - 2)) {
+					DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+					TimeSpan diff = dt - origin;
+					return Math.Floor((diff.TotalSeconds - TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now).Seconds));
+				}
+			}
+			return 0;
+		}
+
+		#endregion
+	}
+}

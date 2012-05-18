7687ae64141b886076381c375ef25819484fe184
Maximilian Krauss <max@kraussz.com>
2012-05-18 21:10:54 +0200

    Blank Template for Aboutdialog

---
+++ wunderbar.App/Ui/Dialogs/aboutDialog.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace wunderbar.App.Ui.Dialogs {
+	public partial class aboutDialog : Window {
+		private static aboutDialog _instance;
+
+		public aboutDialog() {
+			InitializeComponent();
+		}
+
+		public static aboutDialog Instance {
+			get { return _instance ?? (_instance = new aboutDialog()); }
+		}
+
+	}
+}

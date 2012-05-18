49f2770542bea1b2c4af57449ac3d268375938f2
Maximilian Krauss <max@kraussz.com>
2012-05-18 21:12:41 +0200

    Added Dialog for edit and view a sigle task

---
+++ wunderbar.App/Ui/Dialogs/taskDialog.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using wunderbar.App.Core;
+using System.Collections;
+
+namespace wunderbar.App.Ui.Dialogs {
+	/// <summary>
+	/// Interaction logic for taskDialog.xaml
+	/// </summary>
+	public partial class taskDialog : Window {
+		public taskDialog() {
+			InitializeComponent();
+		}
+
+		public IEnumerable ListsItemSource { get { return cboLists.ItemsSource; } set { cboLists.ItemsSource = value; } }
+
+		private void btnClose_Click(object sender, RoutedEventArgs e) {
+			Close();
+		}
+	}
+}

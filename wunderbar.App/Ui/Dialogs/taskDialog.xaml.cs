4bf82efde7531955fe49b69e8da0be45282fc0b1
Maximilian Krauss <max@kraussz.com>
2012-05-19 17:54:18 +0200

    Postpone and "mark as done" implemented

--- wunderbar.App/Ui/Dialogs/taskDialog.xaml.cs
+++ wunderbar.App/Ui/Dialogs/taskDialog.xaml.cs
@@ -1,30 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using wunderbar.App.Core;
+﻿using System.Windows;
 using System.Collections;
+using wunderbar.Api.dataContracts;
 
 namespace wunderbar.App.Ui.Dialogs {
-	/// <summary>
-	/// Interaction logic for taskDialog.xaml
-	/// </summary>
 	public partial class taskDialog : Window {
 		public taskDialog() {
 			InitializeComponent();
 		}
 
-		public IEnumerable ListsItemSource { get { return cboLists.ItemsSource; } set { cboLists.ItemsSource = value; } }
+		public IEnumerable ListsItemSource {
+			set { cboLists.ItemsSource = value; } }
 
 		private void btnClose_Click(object sender, RoutedEventArgs e) {
+			Close();
+		}
+
+		private void btnDone_Click(object sender, RoutedEventArgs e) {
+			(DataContext as taskType).markAsDone();
+			Close();
+		}
+
+		private void btnPostpone_Click(object sender, RoutedEventArgs e) {
+			(DataContext as taskType).Postpone();
 			Close();
 		}
 	}

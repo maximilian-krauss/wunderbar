9076888387ebbf5db551be20c2340f70ab1d074f
Maximilian Krauss <max@coffeeinjection.com>
2012-05-29 20:59:39 +0200

    Closes calendar after selection

--- wunderbar.App/Ui/Dialogs/taskDialog.xaml.cs
+++ wunderbar.App/Ui/Dialogs/taskDialog.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Collections;
 using wunderbar.Api.dataContracts;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 
 namespace wunderbar.App.Ui.Dialogs {
 	public partial class taskDialog : Window {
@@ -32,6 +35,12 @@ namespace wunderbar.App.Ui.Dialogs {
 				btnPostpone.Visibility = Visibility.Hidden;
 				btnDone.Visibility = Visibility.Hidden;
 			}
+
+			//Show the postpone only, if the task is due or overdue
+			btnPostpone.Visibility = ((DataContext as taskType).canPostpone)
+			                         	? Visibility.Visible
+			                         	: Visibility.Hidden;
+
 		}
 
 		private void Button_Click(object sender, RoutedEventArgs e) {
@@ -40,6 +49,10 @@ namespace wunderbar.App.Ui.Dialogs {
 
 		private void btnNoDate_Click(object sender, RoutedEventArgs e) {
 			(DataContext as taskType).Date = 0;
+		}
+
+		private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e) {
+				(((sender as Calendar).Parent as Border).Parent as Popup).IsOpen = false;
 		}
 	}
 }

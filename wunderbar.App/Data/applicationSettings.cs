41629fbb6989431fe2cd8099e99f53e64aa8fcb8
Maximilian Krauss <max@coffeeInjection.com>
2012-06-03 17:38:40 +0200

    Brand new settingsdialog

--- wunderbar.App/Data/applicationSettings.cs
+++ wunderbar.App/Data/applicationSettings.cs
@@ -7,6 +7,8 @@ using wunderbar.Api.Extensions;
 using System.Xml.Serialization;
 using System.IO;
 using System.Reflection;
+using Microsoft.Win32;
+using System.Windows;
 
 namespace wunderbar.App.Data {
 	public sealed class applicationSettings : baseModel, ICloneable {
@@ -30,19 +32,41 @@ namespace wunderbar.App.Data {
 			_showDueTasksInTrayIcon = true;
 		}
 
-		/// <summary>Gets or Sets the eMail-Address for Wunderlist.</summary>
+		/// <summary>Gets or sets the eMail-Address for Wunderlist.</summary>
 		public string eMail { get { return _email; } set { _email = value; onPropertyChanged("eMail"); } }
 
-		/// <summary>Gets or Sets the Wunderlist Password.</summary>
+		/// <summary>Gets or sets the Wunderlist password.</summary>
 		public string Password { get { return _password; } set { _password = value; onPropertyChanged("Password"); } }
 
+		/// <summary>Gets or sets whether tasks and lists should synchronize in background or not.</summary>
 		public bool enableAutoSync { get { return _enableAutoSync; } set { _enableAutoSync = value; onPropertyChanged("enableAutoSync"); } }
 
+		/// <summary>Gets or sets the sync-interval in minutes.</summary>
 		public int autoSyncInterval { get { return _autoSyncInterval; } set { _autoSyncInterval = value; onPropertyChanged("autoSyncInterval"); } }
 
+		/// <summary>Gets or sets whether due tasks should appear inside the trayicon or not.</summary>
 		public bool showDueTasksInTrayIcon { get { return _showDueTasksInTrayIcon; } set { _showDueTasksInTrayIcon = value; onPropertyChanged("showDueTasksInTrayIcon"); } }
 
+		/// <summary>Gets or sets whether HTTP-Requets should use NTLM-auth or not. (Required for auth on ISA-Servers)</summary>
 		public bool useNtlmProxyAuthentication { get { return _useNtlmProxyAuthentication; } set { _useNtlmProxyAuthentication = value; onPropertyChanged("useNtlmProxyAuthentication"); } }
+
+		/// <summary>Gets or sets whether wunderbar should run on startup or not.</summary>
+		[XmlIgnore]
+		public bool autoRun {
+			get { return (string) runHive.GetValue(Session.applicationName, string.Empty) == Assembly.GetExecutingAssembly().Location; }
+			set {
+				if(value)
+					runHive.SetValue(Session.applicationName, Assembly.GetExecutingAssembly().Location);
+				else
+					runHive.DeleteValue(Session.applicationName, false);
+				
+				onPropertyChanged("autoRun");
+			}
+		}
+
+		private RegistryKey runHive {
+			get { return Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", true); }
+		}
 
 		[XmlIgnore] //the Property is Private but just for being save we don't want to serialize the applicationSession
 		private applicationSession Session { get; set; }
